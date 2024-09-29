53096f0d4105845f2e62a433318101e4a9bea94a
Kenobi98 <majilakartik123@gmail.com>
2024-09-29 11:06:45 +0530

    Added new models

--- analysis/params.py
+++ analysis/params.py
@@ -7,31 +7,32 @@ def parameter_files():
 		"interaction": {
 			# Multi output interaction versions with vanilla block.
 			"cg_1": [
-				"Epsilon_3_0",
-				"model_global_-[256, 'ln2', True, 1, '']_[0, 3, 0, 2, 'vanilla', '']_['elu', None]_0.0004_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.98__0"
+				"Epsilon_3_6.2",
+				"model_global-[256, 'ln2', True, 1, '']_[0, 3, 0, 2, 'vanilla', '']_['elu', None]_0.0004_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.98__0"
 			],
 			"cg_5": [
-				"Epsilon_3_0.1",
-				"model_global_-[128, 'ln2', True, 1, '']_[0, 3, 0, 2, 'vanilla', '']_['elu', None]_0.0002_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.97__0"
+				"Epsilon_3_6.1",
+				"model_global-[128, 'ln2', True, 1, '']_[0, 3, 0, 2, 'vanilla', '']_['elu', None]_0.0002_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.97__0"
 			],
 			"cg_10": [
-				"Epsilon_3_0.2",
-				"model_global_-[128, 'ln2', True, 1, '']_[0, 3, 0, 2, 'vanilla', '']_['elu', None]_0.0001_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.97__0"
+				"Epsilon_3_6",
+				"model_global-[128, 'ln2', True, 1, '']_[0, 3, 0, 2, 'vanilla', '']_['elu', None]_0.0001_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.97__0"
 			],
 		},
 		"interface": {
 			# Interface versions with vanilla block.
 			"cg_1": [
-				"Epsilon_3_1",
-				"model_global_-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0002_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.98__0"
+				"Epsilon_3_5.2",
+				"model_global-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0002_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.98__0"
+				
 			],
 			"cg_5": [
-				"Epsilon_3_1.1",
-				"model_global_-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0001_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.94__0"
+				"Epsilon_3_5.1",
+				"model_global-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0001_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.94__0"
 			],
 			"cg_10": [
-				"Epsilon_3_1.2",
-				"model_global_-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0001_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.93__0"
+				"Epsilon_3_5",
+				"model_global-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0001_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.93__0"
 			]
 		}
 	}
