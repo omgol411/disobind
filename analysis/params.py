720ca59a1b537616bd56ac18ec1eb5d67e8b9d56
Kenobi98 <majilakartik123@gmail.com>
2025-05-29 10:01:56 +0530

    add avg2d params

--- analysis/params.py
+++ analysis/params.py
@@ -40,14 +40,10 @@ def get_interaction_params_dict( model_version: int ) -> Dict:
 			"cg_5": [
 				"Epsilon_3_10.6",
 				"model_global-[256, 'ln2', True, 1, '']_[0, 6, 0, 2, 'vanilla', '']_['elu', None]_0.0002_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.95__0"
-				# "Epsilon_3_10.4",
-				# "model_global-[128, 'ln2', True, 1, '']_[0, 6, 0, 2, 'vanilla', '']_['elu', None]_0.0004_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.94__0"
 			],
 			"cg_10": [
 				"Epsilon_3_10.9",
 				"model_global-[64, 'ln2', True, 1, '']_[0, 6, 0, 2, 'vanilla', '']_['elu', None]_0.0004_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.98__0"
-				# "Epsilon_3_10.5",
-				# "model_global-[128, 'ln2', True, 1, '']_[0, 6, 0, 2, 'vanilla', '']_['elu', None]_0.0003_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.87__0"
 			],
 		}
 	elif model_version == 19:
@@ -97,15 +93,21 @@ def get_inteface_params_dict( model_version: int ) -> Dict:
 		interface_param = {
 			# v_21 params -------------
 			"cg_1": [
+				# "Epsilon_3_14",
+				# "model_global-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0003_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.98__0"
 				"Epsilon_3_11.1",
 				"model_global-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0002_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.95__0"
 				
 			],
 			"cg_5": [
+				# "Epsilon_3_14.1",
+				# "model_global-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0002_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.94__0"
 				"Epsilon_3_11.3",
 				"model_global-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0001_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.92__0"
 			],
 			"cg_10": [
+				# "Epsilon_3_14.2",
+				# "model_global-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0002_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.9__0"
 				"Epsilon_3_11.4",
 				"model_global-[128, 'ln2', True, 1, '']_[0, 0, 0, 0, 'vanilla', '']_['elu', None]_0.0001_0.5_[0.9, 3]_[0.2, 0, 0, 0, 0]_0.05_0.89__0"
 			]
