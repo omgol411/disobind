8e844ab2104d82e2d6351d52babc12a6ab8a62af
Kenobi98 <majilakartik123@gmail.com>
2025-06-03 10:55:00 +0530

    added vote based analysis

--- analysis/idppi_preds2.py
+++ analysis/idppi_preds2.py
@@ -8,6 +8,7 @@ import pandas as pd
 from omegaconf import OmegaConf
 
 import torch
+from torch import nn
 
 from dataset.create_input_embeddings import Embeddings
 from src.models.get_model import get_model
@@ -24,6 +25,7 @@ class IdppiPreds():
 		self.base_dir = os.path.abspath( "./" )
 		self.data_dir = "../database/idppi/"
 		self.output_dir = "./idppi_preds/"
+		self.emb_dir = os.path.join( self.output_dir, "embeddings" )
 		
 		self.idppi_input_file = os.path.join( self.data_dir, "IDPPI_input_diso.csv" )
 		self.idppi_targets_file = os.path.join( self.data_dir, "IDPPI_target.json" )
@@ -31,6 +33,7 @@ class IdppiPreds():
 		self.model_version = 19
 		self.batch_size = 250
 		self.max_len = 200
+		self.cg_model = "interface_1"
 		# Threshold to define a contact from Disobind output.
 		self.contact_threshold = 0.5
 		self.device = "cuda"
@@ -44,14 +47,10 @@ class IdppiPreds():
 
 		self.uniprot_seq_file = os.path.join( self.data_dir, "Uniprot_seq_idppi.json" )
 		self.emb_fasta_file_prefix = "idppi_emb_fasta"
-		# os.path.join( os.path.abspath( self.output_dir ),
-		# 									"idppi_emb_fasta.fasta" )
 		self.emb_file_prefix = "idppi_embeddings"
-		# os.path.join( os.path.abspath( self.output_dir ),
-		# 									"idppi_embeddings.h5" )
-		self.predictions_file = os.path.join( self.output_dir, "Disobind_Predictions_idppi.npy" )
-		# self.predictions_file = "/data2/kartik/Disorder_Proteins/disobind/analysis/idppi_preds/Disobind_Predictions_idppi.npy"
-		self.output_dict_file = os.path.join( self.output_dir, "Results_IDPPI.csv" )
+		self.predictions_file = os.path.join( self.output_dir, f"Disobind_Predictions_{self.cg_model}_idppi.npy" )
+		self.output_score_file = os.path.join( self.output_dir, f"Results_score_IDPPI_{self.cg_model}.csv" )
+		self.output_vote_file = os.path.join( self.output_dir, f"Results_vote_IDPPI_{self.cg_model}.csv" )
 
 
 	def forward( self ):
@@ -69,9 +68,10 @@ class IdppiPreds():
 			toc = time.time()
 			print( f"Total time taken for Disobind prediction = {( toc-tic )/60} minutes" )
 
-		output_dict = self.analysis_idppi()
+		score_output_dict = self.score_analysis_idppi()
+		vote_output_dict = self.vote_analysis_idppi()
 
-		self.save_results( output_dict )
+		self.save_results( score_output_dict, vote_output_dict )
 
 		print( "May the Force be with you..." )
 
@@ -81,6 +81,7 @@ class IdppiPreds():
 		Create the required dirs.
 		"""
 		os.makedirs( self.output_dir, exist_ok = True )
+		os.makedirs( self.emb_dir, exist_ok = True )
 
 
 	def read_csv( self ):
@@ -152,9 +153,9 @@ class IdppiPreds():
 			batch = entry_ids[start:end]
 
 			print( f"Running predictions for batch {start}-{end}..." )
-			emb_fasta_file = os.path.join( os.path.abspath( self.output_dir ),
+			emb_fasta_file = os.path.join( os.path.abspath( self.emb_dir ),
 											f"{self.emb_fasta_file_prefix}_{start}-{end}.fasta" )
-			emb_file = os.path.join( os.path.abspath( self.output_dir ),
+			emb_file = os.path.join( os.path.abspath( self.emb_dir ),
 											f"{self.emb_file_prefix}_{start}-{end}.h5" )
 			batch_preds = self.get_preds_for_batch( model, batch, emb_fasta_file, emb_file )
 			self.predictions.update( batch_preds )
@@ -185,19 +186,8 @@ class IdppiPreds():
 		torch.cuda.empty_cache()
 		time.sleep( 5 )
 
-		subprocess.call( ["rm", f"{emb_file}", f"{emb_fasta_file}"] )
+		# subprocess.call( ["rm", f"{emb_file}", f"{emb_fasta_file}"] )
 		return batch_preds
-
-
-	# def update_predictions_dict( self, batch_preds: Dict ):
-	# 	"""
-	# 	Update predictions for all entry_ids to the respective pair_id.
-	# 	"""
-	# 	for pair_id in batch_preds:
-	# 		if pair_id not in self.predictions:
-	# 			self.predictions[pair_id] = {}
-	# 		for entry_id in batch_preds[pair_id]:
-	# 			self.predictions[pair_id][entry_id] = batch_preds[pair_id][entry_id].copy()
 
 	################################################################################
 	################################################################################
@@ -205,7 +195,8 @@ class IdppiPreds():
 		"""
 		Load pre-trained model in evaluation mode for making predictions.
 		"""
-		mod_ver = self.parameters["interface"]["cg_1"][0]
+		task, cg = self.cg_model.split( "_" )
+		mod_ver = self.parameters[task][f"cg_{cg}"][0]
 		model_config = f"../params/Model_config_{mod_ver}.yml"
 		model_config = OmegaConf.load( model_config )
 		model_config = model_config.conf.model_params
@@ -216,7 +207,8 @@ class IdppiPreds():
 		mod = "_".join( m[:-1] )
 		# model version.
 		ver = m[-1]
-		params_file = self.parameters["interface"]["cg_1"][1]
+		
+		params_file = self.parameters[task][f"cg_{cg}"][1]
 		# Load Disobind model.
 		model = get_model( model_config ).to( self.device )
 		model.load_state_dict( 
@@ -228,34 +220,39 @@ class IdppiPreds():
 		return model
 
 
+	def coarse_grain_embedding( self, prot_emb: torch.Tensor ) -> torch.Tensor:
+		"""
+		Coarse grain the given the residue-level protein embedding.
+		prot_emb -> {N,L,C}; where N = 1, L = protein length, C = feature dimension.
+		Due to pooling, some end residues might be lost.
+		"""
+		_, cg = self.cg_model.split( "_" )
+		cg = int( cg )
+		if cg == 1:
+			cg_embedding = prot_emb
+		else:
+			m = nn.MaxPool1d( kernel_size = cg, stride = cg )
+			embedding = prot_emb.clone()
+			# [N,L,C] -> [N,C,L]
+			embedding = torch.permute( embedding, ( 0, 2, 1 ) )
+			cg_embedding = m( embedding )
+			# [N,C,L] -> [N,L,C]
+			cg_embedding = torch.permute( cg_embedding, ( 0, 2, 1 ) )
+
+		return cg_embedding
+
+
 	def prepare_emb_tensor( self, prot_emb: np.array ) -> torch.Tensor:
 		"""
 		Given the input prot embedding:
 			Pad to max_len.
 			Convert to torch.Tensor.
 		"""
-		# prot1 --> [L,C]; K -> protein length, C -> feature dim.
-		# m, n = prot_emb.shape
-		# mask = np.zeros( [self.max_len, n] )
-
-		# mask[:m,:] = prot_emb
-		# prot_emb = mask
 		prot_emb = torch.from_numpy( prot_emb
 							).unsqueeze( 0 ).float()
+		prot_emb = self.coarse_grain_embedding( prot_emb )
+
 		return prot_emb.to( self.device )
-
-
-	def create_output_mask( self, p1_len: int, p2_len: int ) -> np.array:
-		"""
-		Given lengths for prot1/2, create a binary output mask for
-			interface_1 prediction.
-		"""
-		p1_mask = np.zeros( [self.max_len] )
-		p1_mask[:p1_len] = 1
-		p2_mask = np.zeros( [self.max_len] )
-		p2_mask[:p2_len] = 1
-		output_mask = np.concatenate( [p1_mask,p2_mask], axis = 0 )
-		return output_mask
 
 
 	def predict( self, model, prot1_emb_dict: Dict, prot2_emb_dict: Dict ):
@@ -266,16 +263,11 @@ class IdppiPreds():
 			Get model prediction.
 		Predictions are stored as:
 			preds_dict ={
-				"pair_id": {
-					"entry_id": 
-				}
+				"entry_id": {}
 			}
 		"""
 		preds_dict = {}
 		for entry_id in prot1_emb_dict:
-			# pair_id = self.get_pair_id_from_entry_id( entry_id )
-			# if pair_id not in preds_dict:
-			# 	preds_dict[pair_id] = {}
 
 			p1_emb = prot1_emb_dict[entry_id]
 			p2_emb = prot2_emb_dict[entry_id]
@@ -283,36 +275,36 @@ class IdppiPreds():
 			p1_len = p1_emb.shape[0]
 			p2_len = p2_emb.shape[0]
 
+			interaction_mask = torch.ones( [p1_len, p2_len] ).unsqueeze( 0 ).unsqueeze( -1 )
+			interaction_mask = interaction_mask.to( self.device ).float()
+
 			prot1_emb = self.prepare_emb_tensor( p1_emb )
 			prot2_emb = self.prepare_emb_tensor( p2_emb )
 
 			with torch.no_grad():
-				pred_interface = model( prot1_emb, prot2_emb )
+				pred_interface = model( prot1_emb, prot2_emb, interaction_mask )
 				pred_interface = pred_interface.squeeze( 0 ).detach().cpu().numpy()
-				# output_mask = self.create_output_mask( p1_len, p2_len )
-				# pred_interface = pred_interface*output_mask
 			preds_dict[entry_id] = pred_interface
 		return preds_dict
 
 	################################################################################
 	################################################################################
-	def analysis_idppi( self ):
+	def score_analysis_idppi( self ):
 		"""
 		Given Disobind interface_1 predictions, determine
-			if the protein pair interacts or not (PPI task).
+			if the protein pair interacts or not (PPI task) using Disobind scores.
 		"""
-		print( "\nIDPPI analysis..." )
+		print( "\nScore based IDPPI analysis..." )
 		output_dict = {k:[] for k in ["test_name", "contact_threshold", "Recall", "Precision",
 										"F1score", "AvgPrecision", "MCC", "AUROC", "Accuracy"]}
 		with open( self.idppi_targets_file, "r" ) as f:
 			targets_dict = json.load( f )
 
 		for ct in [0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.975, 0.99]:
-		# for ct in [0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 0.995, 0.999, 0.9999]:
 			self.contact_threshold = ct
 			print( f"Contact threshold = {self.contact_threshold}..." )
 
-			ppi_dict = self.get_ppi_dict()
+			ppi_dict = self.get_ppi_dict1()
 			preds_dict, targets = self.prepare_pred_target_tensors( ppi_dict, targets_dict )
 
 			for test_name in preds_dict:
@@ -321,6 +313,42 @@ class IdppiPreds():
 
 				output_dict["test_name"].append( test_name )
 				output_dict["contact_threshold"].append( ct )
+				for i, metric in enumerate( ["Recall", "Precision", "F1score", "AvgPrecision",
+												"MCC", "AUROC", "Accuracy"] ):
+					output_dict[metric].append( metrics[i] )
+				print( metrics )
+			for k in output_dict:
+				output_dict[k].append( "" )
+			print( "\n------------------------------------\n" )
+		return output_dict
+
+
+	def vote_analysis_idppi( self ):
+		"""
+		Given Disobind interface_1 predictions, determine
+			if the protein pair interacts or not (PPI task)
+			using no. of interface residues.
+		"""
+		print( "\nVote based IDPPI analysis..." )
+		output_dict = {k:[] for k in ["test_name", "vote_threshold", "Recall", "Precision",
+										"F1score", "AvgPrecision", "MCC", "AUROC", "Accuracy"]}
+		with open( self.idppi_targets_file, "r" ) as f:
+			targets_dict = json.load( f )
+
+		self.contact_threshold = 0.5
+		for vote_threshold in np.arange( 0.0, 0.055, 0.005 ):
+			
+			print( f"Vote threshold = {self.contact_threshold}..." )
+
+			ppi_dict = self.get_ppi_dict2( vote_threshold )
+			preds_dict, targets = self.prepare_pred_target_tensors( ppi_dict, targets_dict )
+
+			for test_name in preds_dict:
+				print( f"{test_name} ---------" )
+				metrics = self.calculate_metrics( preds_dict[test_name], targets )
+
+				output_dict["test_name"].append( test_name )
+				output_dict["vote_threshold"].append( vote_threshold )
 				for i, metric in enumerate( ["Recall", "Precision", "F1score", "AvgPrecision",
 												"MCC", "AUROC", "Accuracy"] ):
 					output_dict[metric].append( metrics[i] )
@@ -357,7 +385,7 @@ class IdppiPreds():
 		return preds_dict, targets
 
 
-	def get_ppi_dict( self ):
+	def get_ppi_dict1( self ):
 		"""
 		Test different ways to obtain PPI predictions.
 		Store results for each test in ppi_dict.
@@ -365,8 +393,7 @@ class IdppiPreds():
 		ppi_dict = {}
 		for entry_id in self.predictions:
 			for test in [self.interface_to_ppi_1, self.interface_to_ppi_2,
-						self.interface_to_ppi_3, self.interface_to_ppi_4,
-						self.interface_to_ppi_5, self.interface_to_ppi_6]:
+						self.interface_to_ppi_3, self.interface_to_ppi_4]:
 				test_name, ppi_pred = test( entry_id )
 				if test_name not in ppi_dict:
 					ppi_dict[test_name] = {entry_id: ppi_pred}
@@ -375,49 +402,23 @@ class IdppiPreds():
 		return ppi_dict
 
 
+	def get_ppi_dict2( self, vote_threshold: float ):
+		"""
+		Test different ways to obtain PPI predictions.
+		Store results for each test in ppi_dict.
+		"""
+		ppi_dict = {}
+		for entry_id in self.predictions:
+			for test in [self.interface_to_ppi_5, self.interface_to_ppi_6]:
+				test_name, ppi_pred = test( entry_id,vote_threshold )
+				if test_name not in ppi_dict:
+					ppi_dict[test_name] = {entry_id: ppi_pred}
+				else:
+					ppi_dict[test_name][entry_id] = ppi_pred
+		return ppi_dict
+
+
 	def interface_to_ppi_1( self, entry_id: str ):
-		"""
-		'prot_any'
-		A protein pair is interacting if,
-			any( prot1, prot2 )
-			have predicted interface residues.
-		"""
-		name = "prot_any"
-
-		pred_interface = self.predictions[entry_id]
-		pred_interface = np.where( pred_interface > self.contact_threshold, 1, 0 )
-		if np.count_nonzero( pred_interface ) > 0:
-			ppi_pred = 1
-		else:
-			ppi_pred = 0
-		return name, ppi_pred
-
-
-	def interface_to_ppi_2( self, entry_id: str ):
-		"""
-		'prot_all'
-		A protein pair is interacting if,
-			all( prot1, prot2 )
-			have predicted interface residues.
-		"""
-		name = "prot_all"
-
-		pred_interface = self.predictions[entry_id]
-		pred_interface = np.where( pred_interface > self.contact_threshold, 1, 0 )
-
-		( uni_id1, start1, end1,
-			uni_id2, start2, end2, copy_num ) = self.split_entry_id( entry_id, return_pos = True )
-		p1_interface = pred_interface[:end1]
-		p2_interface = pred_interface[end1:]
-
-		if np.count_nonzero( p1_interface ) > 0 and np.count_nonzero( p2_interface ) > 0:
-			ppi_pred = 1
-		else:
-			ppi_pred = 0
-		return name, ppi_pred
-
-
-	def interface_to_ppi_3( self, entry_id: str ):
 		"""
 		'max_score_any'
 		A protein pair is interacting if, max score
@@ -431,7 +432,7 @@ class IdppiPreds():
 		return name, ppi_pred
 
 
-	def interface_to_ppi_4( self, entry_id: str ):
+	def interface_to_ppi_2( self, entry_id: str ):
 		"""
 		'avg_score_any'
 		A protein pair is interacting if, the avg score
@@ -446,7 +447,7 @@ class IdppiPreds():
 		return name, ppi_pred
 
 
-	def interface_to_ppi_5( self, entry_id: str ):
+	def interface_to_ppi_3( self, entry_id: str ):
 		"""
 		'avg_score_all'
 		A protein pair is interacting if, the avg of tghe avg score
@@ -464,7 +465,7 @@ class IdppiPreds():
 		return name, ppi_pred
 
 
-	def interface_to_ppi_6( self, entry_id: str ):
+	def interface_to_ppi_4( self, entry_id: str ):
 		"""
 		'avg_max_score'
 		A protein pair is interacting if, the avg of the max score
@@ -481,6 +482,57 @@ class IdppiPreds():
 		ppi_pred = ( np.max( p1_interface ) + np.max( p2_interface ) )/2
 		return name, ppi_pred
 
+
+	def interface_to_ppi_5( self, entry_id: str, vote_threshold: float ):
+		"""
+		'max_vote'
+		We consider a protein pair to be interacting if,
+			both proteins have > threshold vote.
+		vote is --> no. of interface residues in each protein.
+		"""
+		name = "vote_all"
+		frag_pred = []
+		uni_id1, start1, end1, uni_id2, start2, end2, copy_num = self.split_entry_id( entry_id, return_pos = True )
+		pred_interface = self.predictions[entry_id]
+		p1_len = end1 - start1 + 1
+		p2_len = end2 - start2 + 1
+
+		p1_interface = pred_interface[:end1]
+		p1_vote = np.count_nonzero( np.where( p1_interface > self.contact_threshold, 1, 0 ) )
+		p2_interface = pred_interface[end1:]
+		p2_vote = np.count_nonzero( np.where( p2_interface > self.contact_threshold, 1, 0 ) )
+
+		if p1_vote > vote_threshold*p1_len and p2_vote > vote_threshold*p2_len:
+			ppi_pred = 1
+		else:
+			ppi_pred = 0
+		return name, ppi_pred
+
+
+	def interface_to_ppi_6( self, entry_id: str, vote_threshold: float ):
+		"""
+		'vote_any'
+		We consider a protein pair to be interacting if,
+			either of the proteins have > threshold vote.
+		vote is --> no. of interface residues in each protein.
+		"""
+		name = "vote_any"
+		frag_pred = []
+		uni_id1, start1, end1, uni_id2, start2, end2, copy_num = self.split_entry_id( entry_id, return_pos = True )
+		pred_interface = self.predictions[entry_id]
+		p1_len = end1 - start1 + 1
+		p2_len = end2 - start2 + 1
+
+		p1_interface = pred_interface[:end1]
+		p1_vote = np.count_nonzero( np.where( p1_interface > self.contact_threshold, 1, 0 ) )
+		p2_interface = pred_interface[end1:]
+		p2_vote = np.count_nonzero( np.where( p2_interface > self.contact_threshold, 1, 0 ) )
+
+		if p1_vote > vote_threshold*p1_len or p2_vote > vote_threshold*p2_len:
+			ppi_pred = 1
+		else:
+			ppi_pred = 0
+		return name, ppi_pred
 
 	################################################################################
 	################################################################################
@@ -507,12 +559,16 @@ class IdppiPreds():
 
 	################################################################################
 	################################################################################
-	def save_results( self, output_dict: Dict[str, List] ):
+	def save_results( self, score_output_dict: Dict[str, List],
+							vote_output_dict: Dict[str, List] ):
 		"""
 		Save the results dict as a .csv file.
 		"""
-		df = pd.DataFrame( output_dict )
-		df.to_csv( self.output_dict_file, index = False )
+		df = pd.DataFrame( score_output_dict )
+		df.to_csv( self.output_score_file, index = False )
+
+		df = pd.DataFrame( vote_output_dict )
+		df.to_csv( self.output_vote_file, index = False )
 
 
 if __name__ == "__main__":
